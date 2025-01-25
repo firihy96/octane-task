a180d4dd7636b985403ee9dae02f987c8a0e95b8
Abdullah Firihy <abdullahfirihy96@outlook.com>
2025-01-25 14:44:05 +0200

    creating spinner loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
+  );
+};
+
+export default Loading;
