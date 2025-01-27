89ea9e514e2f86f37507d708090746ba9a32dffe
Abdullah Firihy <abdullahfirihy96@outlook.com>
2025-01-27 07:51:29 +0200

    sty: re-structure and style row selection checkbox comp

--- src/components/table/IndeterminateCheckbox.jsx
+++ src/components/table/IndeterminateCheckbox.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { useRef, useEffect } from "react";
 
 const IndeterminateCheckbox = ({ indeterminate, ...props }) => {
@@ -9,9 +8,36 @@ const IndeterminateCheckbox = ({ indeterminate, ...props }) => {
     if (ref.current) {
       ref.current.indeterminate = !props.checked && indeterminate;
     }
-  }, [ref, indeterminate]);
+  }, [ref, indeterminate, props.checked]);
 
-  return <input type="checkbox" className="cursor-pointer" ref={ref} {...props} />;
+  return (
+    <div className="inline-flex items-center">
+      <label className="flex items-center cursor-pointer relative">
+        <input
+          ref={ref}
+          {...props}
+          type="checkbox"
+          className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800"
+        />
+        <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-3.5 w-3.5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            stroke="currentColor"
+            strokeWidth="1"
+          >
+            <path
+              fillRule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </span>
+      </label>
+    </div>
+  );
 };
 
 export default IndeterminateCheckbox;
